4b71d35abe10e288855bce06b37a6b71d6a56071
Prabhanshu Singh <47481995+Prabhanshu76@users.noreply.github.com>
2024-07-17 12:29:52 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -182,5 +182,9 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify({'message': f'Hello, {current_user}!'})
 
+@app.route("/")
+def sanity_check():
+    return jsonify({"message": "Application is running"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
